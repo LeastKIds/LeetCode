0fb00484b5307fcfb4e0be2f018e4c40b82d3f77
kimjinhong <52005780+LeastKIds@users.noreply.github.com>
2024-03-26 16:10:58 +0900

    Time: 0 ms (100.00%), Space: 2.1 MB (97.30%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.rs
@@ -0,0 +1,39 @@
+impl Solution {
+    pub fn longest_palindrome(s: String) -> i32 {
+        // 조건
+        // 알파벳은 반드시 짝수개만 활용될 수 있음 ex) abccba
+        // 홀수개인 경우 전체 길이는 반드시 홀수개에 가운데에는 홀수개가 들어가야만 함.
+        // 가장 긴 값을 가져야만 함
+
+        let mut alphabet = std::collections::HashMap::new();
+
+        for c in s.chars() {
+            *alphabet.entry(c).or_insert(0) += 1;
+        }
+
+        let mut length = 0;
+        let mut bigOdd = 0;
+        let mut bigOddS = None;
+
+        for (key, value) in alphabet.iter() {
+            if (*value % 2 != 0 && bigOdd < *value) {
+                bigOdd = *value;
+                bigOddS = Some(key);
+            } 
+        }
+
+        if let Some(bigOddChar) = bigOddS {
+            alphabet.insert(*bigOddChar, 0);
+        }
+
+        for (_key, value) in alphabet.iter_mut() {
+            if (*value % 2 == 0) {
+                length += *value;
+            } else {
+                length += *value - 1;
+            }
+        }
+
+        return length + bigOdd;
+    }
+}
